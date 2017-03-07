6038cdf44acd81d08ba2536acab239ecba9b3930
Eva Lai <evalai.dc@gmail.com>
2017-03-07 14:14:19 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+// (function(){
+//   angular.module("wdinstagram", [])
+// })();
+
+angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("GramIndexController", [
+    GramIndexControllerFunction
+  ])
+
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("GramIndex", {
+      url: "/grams",
+      templateUrl: "js/ng-views/index.html",
+      controller: "GramIndexController",
+      controllerAs: "vm"
+    })
+  }
+
+
+  function GramIndexControllerFunction()
