a7cffde215cf0736a17f7e34ef6dadf94489f2a9
Eva Lai <evalai.dc@gmail.com>
2017-03-07 23:09:15 -0500

    router setup correctly

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
 "use strict";
 
-// (function(){
-//   angular.module("wdinstagram", [])
-// })();
+
+
+let gramData = [
+  {photo_url: "http://yhwiki.com/images/thumb/8/86/Power-rangers-megaforce.jpg/300px-Power-rangers-megaforce.jpg" , author: "Eric", body: "Power Rangers MegaForce"},
+  {photo_url: "http://yhwiki.com/images/thumb/2/2c/Power-rangers-dino-charge.jpg/300px-Power-rangers-dino-charge.jpg", author: "Bedig", body: "Power Rangers Dino Charge"},
+  {photo_url: "https://i.ytimg.com/vi/dW-HTrFcEYI/maxresdefault.jpg", author: "Hannah", body: "Mighty Morphin Power Rangers"},
+  {photo_url: "https://s-media-cache-ak0.pinimg.com/736x/e6/16/87/e61687196107e965494c54cc0094a195.jpg", author: "Hab", body: "Power Rangers in Space"},
+
+]
+
+
 
 angular
-  .module("wdinstagram", [
-    "ui.router",
-    "ngResource"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ])
+  .module("wdinstagramApp", ['ui.router'])
+  .config(["$stateProvider", RouterFunction])
   .controller("GramIndexController", [
     GramIndexControllerFunction
   ])
 
 
   function RouterFunction($stateProvider){
-    $stateProvider
-    .state("GramIndex", {
-      url: "/grams",
-      templateUrl: "js/ng-views/index.html",
-      controller: "GramIndexController",
-      controllerAs: "vm"
-    })
+    console.log("Router setup correctly")
+    // $stateProvider
+    // .state("GramIndex", {
+    //   url: "/grams",
+    //   templateUrl: "js/ng-views/index.html",
+    //   controller: "GramIndexController",
+    //   controllerAs: "vm"
+    // })
   }
 
 
-  function GramIndexControllerFunction()
+  function GramIndexControllerFunction(gramData) {
+    this.grams = gramData
+  }
