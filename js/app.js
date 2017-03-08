bfcf0ad265584669c4cf416eed6c207b534a90e1
Eva Lai <evalai.dc@gmail.com>
2017-03-08 16:32:23 -0500

    added show capaibility for each entry/gram

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@
 
 
 let gramData = [
-  {photo_url: "http://yhwiki.com/images/thumb/8/86/Power-rangers-megaforce.jpg/300px-Power-rangers-megaforce.jpg" , author: "Eric", body: "Power Rangers MegaForce"},
-  {photo_url: "http://yhwiki.com/images/thumb/2/2c/Power-rangers-dino-charge.jpg/300px-Power-rangers-dino-charge.jpg", author: "Bedig", body: "Power Rangers Dino Charge"},
-  {photo_url: "https://i.ytimg.com/vi/dW-HTrFcEYI/maxresdefault.jpg", author: "Hannah", body: "Mighty Morphin Power Rangers"},
-  {photo_url: "https://s-media-cache-ak0.pinimg.com/736x/e6/16/87/e61687196107e965494c54cc0094a195.jpg", author: "Hab", body: "Power Rangers in Space"}
+  {id: 0, photo_url: "http://yhwiki.com/images/thumb/8/86/Power-rangers-megaforce.jpg/300px-Power-rangers-megaforce.jpg" , author: "Eric", body: "Power Rangers MegaForce"},
+  {id: 1, photo_url: "http://yhwiki.com/images/thumb/2/2c/Power-rangers-dino-charge.jpg/300px-Power-rangers-dino-charge.jpg", author: "Bedig", body: "Power Rangers Dino Charge"},
+  {id: 2, photo_url: "https://i.ytimg.com/vi/dW-HTrFcEYI/maxresdefault.jpg", author: "Hannah", body: "Mighty Morphin Power Rangers"},
+  {id: 3, photo_url: "https://s-media-cache-ak0.pinimg.com/736x/e6/16/87/e61687196107e965494c54cc0094a195.jpg", author: "Hab", body: "Power Rangers in Space"}
 
 ]
 
@@ -18,6 +18,10 @@ angular
   .controller("GramIndexController", [
     GramIndexControllerFunction
   ])
+  .controller("GramShowController", [
+    "$stateParams",
+    GramShowControllerFunction
+  ])
 
 
   function RouterFunction($stateProvider){
@@ -28,9 +32,19 @@ angular
       controller: "GramIndexController",
       controllerAs: "vm"
     })
+    .state("gramShow", {
+      url: "/grams/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "GramShowController",
+      controllerAs: "vm"
+    })
   }
 
 
   function GramIndexControllerFunction() {
     this.grams = gramData;
+  }
+
+  function GramShowControllerFunction($stateParams) {
+    this.gram = gramData[$stateParams.id];
   }
